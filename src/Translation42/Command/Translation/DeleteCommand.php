d1fea15a2f8dd496278bd2c09dc0ae2e8d5e6eea
luchaos <lcs@luchaos.at>
2015-06-02 17:17:30 +0200

    delete command

--- src/Translation42/Command/Translation/DeleteCommand.php
+++ src/Translation42/Command/Translation/DeleteCommand.php
@@ -1,24 +1,63 @@
 <?php
 /**
- * admin42 (www.raum42.at)
+ * translation42 (www.raum42.at)
  *
  * @link      http://www.raum42.at
  * @copyright Copyright (c) 2010-2014 raum42 OG (http://www.raum42.at)
  *
  */
 
-namespace Admin42\Command\Translation;
+namespace Translation42\Command\Translation;
 
-use Admin42\Model\Translation;
+use Translation42\Model\Translation;
 use Core42\Command\AbstractCommand;
 
 class DeleteCommand extends AbstractCommand
 {
     /**
+     * @var Translation
+     */
+    private $translation;
+
+    /**
+     * @var int
+     */
+    private $translationId;
+
+    /**
+     * @param Translation $translation
+     * @return $this
+     */
+    public function setTranslation(Translation $translation)
+    {
+        $this->translation = $translation;
+
+        return $this;
+    }
+
+    /**
+     * @param int $translationId
+     * @return $this
+     */
+    public function setTranslationId($translationId)
+    {
+        $this->translationId = $translationId;
+
+        return $this;
+    }
+
+    /**
      *
      */
     protected function preExecute()
     {
+        if (!empty($this->translationId)) {
+            $this->translation = $this->getTableGateway('Admin42\User')->selectByPrimary((int)$this->translationId);
+        }
+
+        if (!($this->translation instanceof Translation)) {
+            $this->addError("translation", "invalid translation");
+        }
     }
 
     /**
@@ -26,5 +65,6 @@ class DeleteCommand extends AbstractCommand
      */
     protected function execute()
     {
+        $this->getTableGateway('Admin42\Translation')->delete($this->translation);
     }
 }
