3d5b13673526ae913515126d5a0638586acc7136
Thomas Payer <payer@raum42.at>
2016-06-04 12:24:02 +0200

    add missing translations

--- data/language/en-US.php
+++ data/language/en-US.php
@@ -20,4 +20,20 @@ return [
 
     'status.auto'   => 'Auto-generated',
     'status.manual' => 'Manual',
+
+    'toaster.translation.create.title.success' => 'Translation added',
+    'toaster.translation.create.message.success' => 'Translation successfully added',
+    'toaster.translation.edit.title.success' => 'Translation saved',
+    'toaster.translation.edit.message.success' => 'Translation successfully saved',
+
+    'toaster.translation.create.title.error' => 'Error',
+    'toaster.translation.create.message.error' => 'There was an error in adding the translation',
+    'toaster.translation.edit.title.error' => 'Error',
+    'toaster.translation.edit.message.error' => 'There was an error in saving the translation',
+
+    'toaster.translation.delete.title.success' => 'Translation deleted',
+    'toaster.translation.delete.message.success' => 'Translation successfully deleted',
+
+    'translation.modal.delete.title' => 'Really delete?',
+    'translation.modal.delete.content' => 'Really delete this Translations?',
 ];
